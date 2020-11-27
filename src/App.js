0c72cccfeb2e2a7722b5fdc58d9e9aa56b913c66
sdubrez <samuel@1kubator.com>
2020-11-27 15:38:18 +0100

    Set <title> on each page & update favicon

--- src/App.js
+++ src/App.js
@@ -27,48 +27,60 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-        <PublicRoute exact component={Home} path={ROUTE_HOME}></PublicRoute>
+        <PublicRoute
+          exact
+          component={Home}
+          path={ROUTE_HOME}
+          title="Home"></PublicRoute>
 
         <PublicRoute
           exact
           component={DungeonPF}
           path={ROUTE_DG_PF}
-          layout={EmptyLayout}></PublicRoute>
+          layout={EmptyLayout}
+          title="Plaguefall"></PublicRoute>
         <PublicRoute
           exact
           component={DungeonTNW}
           path={ROUTE_DG_TNW}
-          layout={EmptyLayout}></PublicRoute>
+          layout={EmptyLayout}
+          title="The necrotic wake"></PublicRoute>
         <PublicRoute
           exact
           component={DungeonSoA}
           path={ROUTE_DG_SOA}
-          layout={EmptyLayout}></PublicRoute>
+          layout={EmptyLayout}
+          title="Spires of Ascension"></PublicRoute>
         <PublicRoute
           exact
           component={DungeonToP}
           path={ROUTE_DG_TOP}
-          layout={EmptyLayout}></PublicRoute>
+          layout={EmptyLayout}
+          title="Theater of Pain"></PublicRoute>
         <PublicRoute
           exact
           component={DungeonMoTS}
           path={ROUTE_DG_MOTS}
-          layout={EmptyLayout}></PublicRoute>
+          layout={EmptyLayout}
+          title="Mists of Tirna Scithe"></PublicRoute>
         <PublicRoute
           exact
           component={DungeonOS}
           path={ROUTE_DG_OS}
-          layout={EmptyLayout}></PublicRoute>
+          layout={EmptyLayout}
+          title="Other side"></PublicRoute>
         <PublicRoute
           exact
           component={DungeonHoA}
           path={ROUTE_DG_HOA}
-          layout={EmptyLayout}></PublicRoute>
+          layout={EmptyLayout}
+          title="Halls of Atonement"></PublicRoute>
         <PublicRoute
           exact
           component={DungeonSD}
           path={ROUTE_DG_SD}
-          layout={EmptyLayout}></PublicRoute>
+          layout={EmptyLayout}
+          title="Sanguine depths"></PublicRoute>
       </Router>
     );
   }
